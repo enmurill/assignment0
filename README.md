76cb3dd92357520ad78059fbbdff50278d2dce4d
Elizabeth Murillo <elizabeth@wireless-169-228-82-27.ucsd.edu>
2019-01-15 11:27:43 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# assignment0
+The primary goal of this assignment is to practice using Github.
+
+We're providing a simple python script that needs to be debugged.
+Your challenge is to fix the code so that it runs correctly and generates the correct
+output. The program itself can tell you when it's working, you just need to correct
+the problems throughout the code. Once the errors are removed, the code will generate
+a text file that is compared to a baseline. If they match, the program reports "True"
+after a successful run. 
+
+## How You're Graded
+1. Demonstrate the ability to clone a github project.
+2. Demonstrate the ability to read python code, identify and fix trivial bugs.
+3. Demonstrate the ability to check your code back into the github.
+
+Programming tip: Good programs work. Great programs are easy to read, self-evident, well-documented,
+and well-tested. Consider this a hint.
+# assignment0
---
+++ assignment.py
@@ -0,0 +1,50 @@
+# -------------------------------------------------------
+# code template for assignment 0
+# This code doesn't do anything meaningful;
+# Just find and remove the bugs to make the final result True
+# -------------------------------------------------------
+
+def matches():
+    with open('output.txt', 'r') as file1:
+        with open('baseline.txt', 'r') as file2:
+
+            line1, line2 = file1.readline(), file2.readline()
+
+            while line1 and line2:
+                if line1 != line2:
+                    return False
+                line1, line2 = file1.readline(), file2.readline()
+    return True
+
+def aGenerator(aLimit):
+    i = 0
+    while i < aLimit:
+        yield i
+        i += 1
+
+def doSomethingElse(anOutputFile):
+    # emit text to the output file...
+    anOutputFile.write("doSomethingElse\n")
+
+    theGen = aGenerator(10)
+
+    anOutputFile.write("word %s\n" % next(theGen))
+    anOutputFile.write("word %s\n" % next(theGen))
+    anOutputFile.write("word %s\n" % next(theGen))
+    anOutputFile.write("words %s\n" % "blah" * next(theGen))
+
+    return True
+
+def doSomething(anOutputFile):
+    anOutputFile.write("doSomething\n")
+    return doSomethingElse(anOutputFile)
+
+# -------------------------------------------------------
+
+if __name__ == "__main__":
+
+    #This code runs the self-test. This code works fine, so you can ignore it...
+    with open('output.txt', 'w') as theOutputFile:
+        doSomething(theOutputFile)
+
+    print("matches ", matches())
